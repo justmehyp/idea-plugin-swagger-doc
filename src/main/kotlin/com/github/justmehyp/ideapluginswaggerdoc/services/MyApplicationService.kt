e4c061440d2987fd62f355ac457b6ead9882f148
GitHub Action <action@github.com>
2022-02-25 04:17:49 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/justmehyp/ideapluginswaggerdoc/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.justmehyp.ideapluginswaggerdoc.services
+
+import com.github.justmehyp.ideapluginswaggerdoc.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/justmehyp/ideapluginswaggerdoc/services/MyProjectService.kt
+++ src/main/kotlin/com/github/justmehyp/ideapluginswaggerdoc/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.justmehyp.ideapluginswaggerdoc.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.justmehyp.ideapluginswaggerdoc.MyBundle
 
 class MyProjectService(project: Project) {
 
